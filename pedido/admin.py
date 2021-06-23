351300207e75763dd7addc235191e0d33740d6c9
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2021-06-23 14:13:36 -0300

    models de pedido

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,17 @@
+from pedido.models import ItemPedido, Pedido
 from django.contrib import admin
 
-# Register your models here.
+
+class ItemPedidoInline(admin.TabularInline):
+    model = ItemPedido
+    extra = 1
+
+
+class PedidoAdmin(admin.ModelAdmin):
+    inlines = [
+        ItemPedidoInline
+    ]
+
+
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(ItemPedido)
--- produto/admin.py
+++ produto/admin.py
@@ -1,5 +1,18 @@
+from django.db import models
 from produto.models import Produto, Variacao
 from django.contrib import admin
 
-admin.site.register(Produto)
+
+class VariacaoInline(admin.TabularInline):
+    model = Variacao
+    extra = 1
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [
+        VariacaoInline
+    ]
+
+
+admin.site.register(Produto, ProdutoAdmin)
 admin.site.register(Variacao)
